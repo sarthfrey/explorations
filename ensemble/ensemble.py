a00ce11ac1f3d80b2a532a174097720387af09d1
sarthfrey <sarth.frey@gmail.com>
2019-06-30 15:48:46 -0700

    spaces

--- ensemble/ensemble.py
+++ ensemble/ensemble.py
@@ -3,50 +3,50 @@ from collections import defaultdict
 
 # Both used for decoration and regular object instantiation
 class _Ensemble(object):
-	model_functions = {}
-	ensemble_groups = defaultdict(set)
-	ensemble = ''
+  model_functions = {}
+  ensemble_groups = defaultdict(set)
+  ensemble = ''
 
-	def __init__(self, specifier, *ensemble_names):
-		model_function = None
-		if callable(specifier):
-			model_function = specifier
-			ensemble_names = set(ensemble_names)
-		else:
-			ensemble_names = set([specifier])
-		if any(not ensemble_name for ensemble_name in ensemble_names):
-			raise ValueError('Must provide a valid ensemble names')
-		if model_function is None:
-			self.ensemble = specifier
-		else:
-			self.ensemble_groups[model_function.__name__] |= ensemble_names
-			self.model_functions[model_function.__name__] = model_function
+  def __init__(self, specifier, *ensemble_names):
+    model_function = None
+    if callable(specifier):
+      model_function = specifier
+      ensemble_names = set(ensemble_names)
+    else:
+      ensemble_names = set([specifier])
+    if any(not ensemble_name for ensemble_name in ensemble_names):
+      raise ValueError('Must provide a valid ensemble names')
+    if model_function is None:
+      self.ensemble = specifier
+    else:
+      self.ensemble_groups[model_function.__name__] |= ensemble_names
+      self.model_functions[model_function.__name__] = model_function
 
-	def __call__(self, *args, **kwargs):
-		model = kwargs.get('model')
-		if callable(model):
-			raise ValueError('HOW')
-			return self.__model
-		else:
-			return self.__model(*args, **kwargs)
+  def __call__(self, *args, **kwargs):
+    model = kwargs.get('model')
+    if callable(model):
+      raise ValueError('HOW')
+      return self.__model
+    else:
+      return self.__model(*args, **kwargs)
 
-	def __model(self, *args, **kwargs):
-		model = kwargs.get('model')
-		if model not in self.model_functions or model not in self.ensemble_groups:
-			raise ValueError('There is no model function `{}` decorated with @Model'.format(model))
-		model_function = self.model_functions[model]
-		ensemble_group = self.ensemble_groups[model]
-		if self.ensemble not in ensemble_group:
-			raise ValueError('Model function `{}` is not attached to ensemble `{}`'.format(model, self.ensemble))
-		kwargs.pop('model', None)
-		return model_function(*args, **kwargs)
+  def __model(self, *args, **kwargs):
+    model = kwargs.get('model')
+    if model not in self.model_functions or model not in self.ensemble_groups:
+      raise ValueError('There is no model function `{}` decorated with @Model'.format(model))
+    model_function = self.model_functions[model]
+    ensemble_group = self.ensemble_groups[model]
+    if self.ensemble not in ensemble_group:
+      raise ValueError('Model function `{}` is not attached to ensemble `{}`'.format(model, self.ensemble))
+    kwargs.pop('model', None)
+    return model_function(*args, **kwargs)
 
 
 def Ensemble(ensemble_name):
-	return _Ensemble(ensemble_name)
+  return _Ensemble(ensemble_name)
 
 
 def Model(*ensemble_names):
-	def wrapper(model_function):
-		return _Ensemble(model_function, *ensemble_names)
-	return wrapper
+  def wrapper(model_function):
+    return _Ensemble(model_function, *ensemble_names)
+  return wrapper
--- ensemble/main.py
+++ ensemble/main.py
@@ -3,25 +3,25 @@ from ensemble import Model, Ensemble
 
 @Model('e1', 'e2')
 def f(x, y=3, z=4):
-	return x + y + z
+  return x + y + z
 
 @Model('e1')
 def g(y):
-	return y**3
+  return y**3
 
 if __name__ == '__main__':
-	e1 = Ensemble('e1')
-	e2 = Ensemble('e2')
-	print(e1(model='f',x=2))
-	print(e1(model='g',y=3))
-	print(e2(model='f',x=2))
-	# catch invalid calls
-	try:
-		print(e2(model='g',y=3))
-	except ValueError:
-		pass
-	try:
-		print(e1(model='h',y=3))
-	except ValueError:
-		pass
-	print(e1(3, model='f'))
+  e1 = Ensemble('e1')
+  e2 = Ensemble('e2')
+  print(e1(model='f',x=2))
+  print(e1(model='g',y=3))
+  print(e2(model='f',x=2))
+  # catch invalid calls
+  try:
+    print(e2(model='g',y=3))
+  except ValueError:
+    pass
+  try:
+    print(e1(model='h',y=3))
+  except ValueError:
+    pass
+  print(e1(3, model='f'))
